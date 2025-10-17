30213f823ad6376021a79976bf5ebbdd89f2fed8
sathish195 <sathishdundigala999@gmail.com>
2025-10-17 16:50:40 +0530

    2

--- redis.js
+++ redis.js
@@ -7,7 +7,9 @@ module.exports = async function (fastify) {
       // host: "redis-13247.c305.ap-south-1-1.ec2.redns.redis-cloud.com",
       // port: 13247,
       // password: "JG8S0vwtOulEpNJPXprPwbrIkj679Yzl",
-      url: `redis://127.0.0.1:6379`,
+      url: "redis://:<JG8S0vwtOulEpNJPXprPwbrIkj679Yzl>@<redis-13247.c305.ap-south-1-1.ec2.redns.redis-cloud.com>:<13247>", // Replace with actual URL
+
+      // url: `redis://127.0.0.1:6379`,
     });
 
     fastify.log.info("✅ Redis connected");
