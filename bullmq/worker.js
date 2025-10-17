ecc3247cfde131ee95d7c287ddbb97f4905d9eeb
sathish195 <sathishdundigala999@gmail.com>
2025-10-17 18:27:57 +0530

    3

--- bullmq/worker.js
+++ bullmq/worker.js
@@ -3,7 +3,8 @@ require("../db")();
 const User = require("../modals/user");
 
 const connection = {
-  url: "redis://127.0.0.1:6379",
+  // url: "redis://127.0.0.1:6379",
+  url: "redis://:<JG8S0vwtOulEpNJPXprPwbrIkj679Yzl>@<redis-13247.c305.ap-south-1-1.ec2.redns.redis-cloud.com>:<13247>", // Replace with actual URL
 };
 
 // Start BullMQ Worker
