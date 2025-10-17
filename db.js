28eec7d28c55b649192090a54ba0a8df2b6d5701
sathish195 <sathishdundigala999@gmail.com>
2025-10-17 18:22:39 +0530

    3

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-// // db.js
-// const mongoose = require("mongoose");
+// db.js
+const mongoose = require("mongoose");
 
-// module.exports = async function connectDB() {
-//   try {
-//     await mongoose.connect(
-//       "mongodb+srv://sathishdundigala999:microlending199@microlending.pnenyi3.mongodb.net/microLending?retryWrites=true&w=majority&appName=microLending",
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     );
-//     console.log("✅ MongoDB connected via Mongoose");
-//   } catch (err) {
-//     console.error("❌ MongoDB connection error:", err.message);
-//     process.exit(1);
-//   }
-// };
+module.exports = async function connectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sathishdundigala999:microlending199@microlending.pnenyi3.mongodb.net/microLending?retryWrites=true&w=majority&appName=microLending",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("✅ MongoDB connected via Mongoose");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
 
 // fastify.register(
 //   require("fastify-postgres"),
@@ -34,14 +34,14 @@
 
 // db/postgres.js
 
-const fp = require("fastify-plugin");
-const fastifyPostgres = require("fastify-postgres");
-console.log("data------> to store");
-async function postgresConnector(fastify, options) {
-  fastify.register(fastifyPostgres, {
-    connectionString: "postgres://postgres:12345@localhost:5432/ecom",
-  });
-}
+// const fp = require("fastify-plugin");
+// const fastifyPostgres = require("fastify-postgres");
+// console.log("data------> to store");
+// async function postgresConnector(fastify, options) {
+//   fastify.register(fastifyPostgres, {
+//     connectionString: "postgres://postgres:12345@localhost:5432/ecom",
+//   });
+// }
 
 // Export as Fastify plugin
-module.exports = fp(postgresConnector);
+// module.exports = fp(postgresConnector);
