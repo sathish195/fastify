35864a38748b6f8ba8aa6ce1c06cd87d70a94aff
sathish195 <sathishdundigala999@gmail.com>
2025-10-21 12:25:40 +0530

    4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./helpers.js/routes");
 const start = async () => {
   try {
     await connectDB(fastify);
-    await connectRedis(fastify); // If you're using Redis
+    // await connectRedis(fastify); // If you're using Redis
     routes(fastify);
 
     const port = process.env.PORT || 4000;
