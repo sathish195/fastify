2797c4bb7463495615ab62699106294729953da4
sathish195 <sathishdundigala999@gmail.com>
2025-10-21 13:07:03 +0530

    adding changes

--- helpers.js/routes.js
+++ helpers.js/routes.js
@@ -2,6 +2,11 @@ const userRoutes = require("../routes/index");
 const productRoutes = require("../routes/products");
 
 async function registerRoutes(fastify, options) {
+  // Default root route
+  fastify.get("/", async (request, reply) => {
+    return { message: "✅ FASTIFY IS RUNNIG!" };
+  });
+
   fastify.register(userRoutes);
   fastify.register(productRoutes);
 }
