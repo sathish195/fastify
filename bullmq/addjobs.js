d7430a6e49882a687e8adc25c9bd8326aee7d121
sathish195 <sathishdundigala999@gmail.com>
2025-10-21 11:37:39 +0530

    4

--- bullmq/addjobs.js
+++ bullmq/addjobs.js
@@ -38,29 +38,29 @@
 // };
 
 // bullmq-setup.js
-const { Queue, QueueEvents } = require("bullmq");
+// const { Queue, QueueEvents } = require("bullmq");
 
-const connection = {
-  url: "redis://:<JG8S0vwtOulEpNJPXprPwbrIkj679Yzl>@<redis-13247.c305.ap-south-1-1.ec2.redns.redis-cloud.com>:<13247>", // Replace with actual URL
+// const connection = {
+//   url: "redis://:<JG8S0vwtOulEpNJPXprPwbrIkj679Yzl>@<redis-13247.c305.ap-south-1-1.ec2.redns.redis-cloud.com>:<13247>", // Replace with actual URL
 
-  // url: "redis://127.0.0.1:6379",
-};
+//   // url: "redis://127.0.0.1:6379",
+// };
 
-module.exports = {
-  queueEvents: new QueueEvents("myqueue", {
-    connection: connection,
-  }),
-  bull: async (data, prior) => {
-    const queue = new Queue("myqueue", {
-      connection: {
-        url: connection,
-      },
-    });
+// module.exports = {
+//   queueEvents: new QueueEvents("myqueue", {
+//     connection: connection,
+//   }),
+//   bull: async (data, prior) => {
+//     const queue = new Queue("myqueue", {
+//       connection: {
+//         url: connection,
+//       },
+//     });
 
-    return queue.add("create_user", data, {
-      priority: prior,
-      removeOnComplete: true,
-      removeOnFail: true,
-    });
-  },
-};
+//     return queue.add("create_user", data, {
+//       priority: prior,
+//       removeOnComplete: true,
+//       removeOnFail: true,
+//     });
+//   },
+// };
