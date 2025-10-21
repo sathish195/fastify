82a09d302647982f33511ec940f8eab21f53da3f
sathish195 <sathishdundigala999@gmail.com>
2025-10-21 12:23:33 +0530

    4

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const routes = require("./helpers.js/routes");
 const start = async () => {
   try {
     await connectDB(fastify);
-    // fastify.register(require("./db"));
-
-    // await connectRedis(fastify);
-    // await worker();
+    await connectRedis(fastify); // If you're using Redis
     routes(fastify);
-    await fastify.listen({ port: 4000 });
-    fastify.log.info("🚀 Server running at http://localhost:4000");
+
+    const port = process.env.PORT || 4000;
+
+    // Correct usage: pass port and host inside an object
+    await fastify.listen({ port: port, host: "0.0.0.0" });
+
+    fastify.log.info(`🚀 Server running at http://0.0.0.0:${port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
+
 start();
